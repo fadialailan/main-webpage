d45e5572ad3a2186ac083631393b8f4f7cf221b0
Fadi Alailan <fadialailan@protonmail.com>
2023-11-19 20:01:28 +0800

    added navbar and toggle colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  site:"https://fadialailan.github.io",
-  base:"/main-webpage",
+  site: "https://fadialailan.github.io",
+  base: "/main-webpage",
+  integrations: [react()]
 });
