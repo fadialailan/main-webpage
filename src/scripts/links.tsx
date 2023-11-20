01b0a837371cba2900edb3e04603c133218d2643
Fadi Alailan <fadialailan@protonmail.com>
2023-11-20 14:31:33 +0800

    wrote more on the home page and organised the links

---
+++ src/scripts/links.tsx
@@ -0,0 +1,4 @@
+export const base_url = import.meta.env.BASE_URL
+export const home_url = base_url + "/"
+export const about_url = base_url + "/about"
+export const icon_url = base_url + "/Source-code-icon.svg"
