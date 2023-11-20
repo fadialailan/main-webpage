89f3e570b0f1461f383966ff6bad7ff74f92c609
Fadi Alailan <fadialailan@protonmail.com>
2023-11-20 17:41:04 +0800

    added picture, social links and changed some words

--- src/scripts/links.tsx
+++ src/scripts/links.tsx
@@ -1,4 +1,13 @@
 export const base_url = import.meta.env.BASE_URL
 export const home_url = base_url + "/"
 export const about_url = base_url + "/about"
+export const contact_url = base_url + "/contact"
 export const icon_url = base_url + "/Source-code-icon.svg"
+export const profile_url = base_url + "/profile.png"
+export const cv_url = "https://drive.proton.me/urls/BJDJVEWM7M#MNCDkg5E8fWS"
+export const email_address = "fadialailan@protonmail.com"
+export const email_url = "mailto:"+email_address
+export const phone_number = "+966 54 665 1007"
+export const github_link = "https://github.com/fadialailan"
+export const gitlab_link = "https://gitlab.com/fadialailan"
+export const linkedin_link = "https://www.linkedin.com/in/fadi-alailan/"
