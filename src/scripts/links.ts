91857fcd0d057aa4fb6d90c0b581c1ca636d4f26
Fadi Alailan <fadialailan@protonmail.com>
2024-06-24 09:21:08 -0400

    added the playground tab

--- src/scripts/links.ts
+++ src/scripts/links.ts
@@ -1,13 +1,23 @@
+// env
 export const base_url = import.meta.env.BASE_URL
+
+// page urls
 export const home_url = base_url + ""
 export const about_url = base_url + "about"
 export const contact_url = base_url + "contact"
+export const playground_url = base_url + "playground"
+
+// resource url
 export const icon_url = base_url + "Source-code-icon.svg"
 export const profile_url = base_url + "profile.png"
+
+// external url
 export const cv_url = "https://drive.proton.me/urls/BJDJVEWM7M#MNCDkg5E8fWS"
-export const email_address = "fadialailan@protonmail.com"
-export const email_url = "mailto:"+email_address
-export const phone_number = "+966 54 665 1007"
 export const github_link = "https://github.com/fadialailan"
 export const gitlab_link = "https://gitlab.com/fadialailan"
 export const linkedin_link = "https://www.linkedin.com/in/fadi-alailan/"
+
+// other
+export const email_address = "fadialailan@protonmail.com"
+export const email_url = "mailto:"+email_address
+export const phone_number = "+966 54 665 1007"
