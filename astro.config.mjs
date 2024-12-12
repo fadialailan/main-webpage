dc6b3fbbbaceda5d0364c9ab191e9c06a6af7fc7
Fadi Alailan <fadialailan@protonmail.com>
2024-12-12 12:55:19 -0500

    added sitemap integration and updated packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fadialailan.uk",
   // base: "/main-webpage",
-  integrations: [react()],
-  prefetch: true,
+  integrations: [react(), sitemap()],
+  prefetch: true
 });
