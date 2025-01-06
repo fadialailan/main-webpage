6b312f80db0b109805b885d83be2509d860bfd10
Fadi Alailan <fadialailan@protonmail.com>
2025-01-06 11:15:59 -0500

    removed old home page from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,28 @@ import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
 
+const site = "https://fadialailan.uk"
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fadialailan.uk",
+  site: site,
   // base: "/main-webpage",
-  integrations: [react(), sitemap()],
-  prefetch: true
+  integrations: [
+    react(),
+    sitemap({
+      filter : function(page) {
+        const removed_pages = [site + "/playground/old_home_page/"]
+        const page_in_removed_pages = removed_pages.includes(page)
+
+        if (page_in_removed_pages) {
+          return false
+        }
+
+        return true
+
+      }
+    })
+  ],
+  prefetch: true,
+
 });
